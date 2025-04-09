4732c8b164a877a6b86a4431a7f440195b4524ca
binod kandel <annomy634@gmail.com>
2025-04-10 00:03:01 +0545

    updated

--- lib/screens/profilepage.dart
+++ lib/screens/profilepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:dostrobajar/provider/profile_provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,19 +12,10 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  final Map<String, dynamic> userData = {
-    'name': 'John Doe',
-    'email': Supabase.instance.client.auth.currentUser?.email ?? '',
-    'phone':
-        Supabase.instance.client.auth.currentUser?.userMetadata?['phone'] ?? '',
-    'address':
-        Supabase.instance.client.auth.currentUser?.userMetadata?['address'] ??
-            'Unknown Address',
-    'listings': 15,
-    'views': 234,
-    'likes': 56,
-    'comments': 28,
-  };
+  @override
+  void initState() {
+    super.initState();
+  }
 
   Widget _buildStatCard(String title, String value, IconData icon) {
     return Container(
@@ -86,134 +79,136 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 16),
-              width: 250,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade200,
-                    blurRadius: 10,
-                    spreadRadius: 2,
-                  )
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                        )
-                      ],
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 4,
-                      ),
-                    ),
-                    child: ClipOval(
-                      child: Image.asset(
-                        'assets/images/pic.gif',
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) => Icon(
-                          Icons.person,
-                          size: 50,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    userData['name'],
-                    style: GoogleFonts.lato(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black,
-                      shadows: [
-                        Shadow(
-                          offset: Offset(1, 1),
-                          blurRadius: 2,
-                          color: Colors.black26,
-                        ),
-                      ],
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Statistics',
-                    style: GoogleFonts.lato(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  GridView.count(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                    children: [
-                      _buildStatCard('Listings',
-                          userData['listings'].toString(), Icons.list_alt),
-                      _buildStatCard('Views', userData['views'].toString(),
-                          Icons.visibility),
-                      _buildStatCard('Likes', userData['likes'].toString(),
-                          Icons.favorite),
-                      _buildStatCard('Comments',
-                          userData['comments'].toString(), Icons.comment),
+      body: Consumer<ProfileProvider>(
+        builder: (context, profileProvider, _) {
+          final userdata = profileProvider.userdata;
+
+          if (userdata == null) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 16),
+                  width: 250,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade200,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                      )
                     ],
                   ),
-                  SizedBox(height: 24),
-                  Text(
-                    'Contact Information',
-                    style: GoogleFonts.lato(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10,
+                              spreadRadius: 2,
+                            )
+                          ],
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 4,
+                          ),
+                        ),
+                        child: ClipOval(
+                          child: CachedNetworkImage(
+                            imageUrl: userdata.imageUrl.replaceAll(' ', ''),
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) => Icon(
+                              Icons.person,
+                              size: 50,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      Text(
+                        '${userdata.firstName} ${userdata.lastName}',
+                        style: GoogleFonts.lato(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(1, 1),
+                              blurRadius: 2,
+                              color: Colors.black26,
+                            ),
+                          ],
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
                   ),
-                  Card(
-                    margin: EdgeInsets.symmetric(vertical: 16),
-                    elevation: 2,
-                    child: Column(
-                      children: [
-                        _buildInfoTile(
-                            'Email', userData['email'], Icons.email_outlined),
-                        Divider(),
-                        _buildInfoTile(
-                            'Phone', userData['phone'], Icons.phone_outlined),
-                        Divider(),
-                        _buildInfoTile('Address', userData['address'],
-                            Icons.location_on_outlined),
-                      ],
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Statistics',
+                        style: GoogleFonts.lato(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        children: [
+                          _buildStatCard('Listings', '0', Icons.list_alt),
+                          _buildStatCard('Views', '0', Icons.visibility),
+                          // _buildStatCard('Likes', '0', Icons.favorite),
+                          // _buildStatCard('Comments', '0', Icons.comment),
+                        ],
+                      ),
+                      SizedBox(height: 24),
+                      Text(
+                        'Contact Information',
+                        style: GoogleFonts.lato(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Card(
+                        margin: EdgeInsets.symmetric(vertical: 16),
+                        elevation: 2,
+                        child: Column(
+                          children: [
+                            _buildInfoTile(
+                                'Email', userdata.email, Icons.email_outlined),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
